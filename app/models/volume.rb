d65de5af9ff45606913b90263e812609455f2269
Thiago Pelizoni <thiago.pelizoni@gmail.com>
2015-10-04 17:21:22 -0300

    Before save, a volume directory will be created. Before delete, a volume directoy will be deleted (I think the second action could be terrible!)

--- app/models/volume.rb
+++ app/models/volume.rb
@@ -1,5 +1,9 @@
 class Volume
   include Mongoid::Document
+
+  before_save :create_volume
+  before_destroy :remove_volume
+
   field :label, type: String
   field :address, type: String
 
@@ -10,5 +14,17 @@ class Volume
 
   def exists?
     File.exists? self.address
+  end
+
+  def create_volume
+    unless File.exist?(self.address)
+      FileUtils.mkdir_p(self.address)
+    end
+  end
+
+  def remove_volume
+    unless File.exist?(self.address)
+      FileUtils.rm_rf(self.address)
+    end
   end
 end
