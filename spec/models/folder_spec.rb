df857cf9dcd09c4fd5f6943793c91902f2d35310
Thiago Pelizoni <thiago.pelizoni@gmail.com>
2015-10-04 19:51:48 -0300

    Folder model tests has been implemented.

--- spec/models/folder_spec.rb
+++ spec/models/folder_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Folder, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    Folder.delete_all
+    @folder = FactoryGirl.build(:folder)
+    @folder.volume = FactoryGirl.build(:volume)
+  end
+
+  it "A Folder must have a volume" do
+    expect(@folder.volume).to be_a Volume
+    @folder.volume = nil
+    expect(@folder.save).to be false
+  end
+
+  it "A Folder must have a valid name" do
+    @folder.name = nil
+    expect(@folder.save).to be false
+  end
+
+  it "A parent Folder must be an instance of Folder" do
+    parent = FactoryGirl.build(:folder)
+    @folder.parent = parent
+    expect(parent).to be_a Folder
+    expect(@folder.save).to be true
+  end
+
+  it "When create a folder a directory must be created in disk inside volume" do
+    expect(@folder.save).to be true
+    expect(File.exists?(@folder.path)).to be true
+  end
+
+  it "When a Folder has been updated, the directory needs to be nenamed" do
+    expect(@folder.save).to be true
+    @folder.name = 'new_folder'
+    expect(@folder.save).to be true
+    #expect(File.exists?(@folder.path)).to be true
+  end
 end
